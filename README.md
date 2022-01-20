0dcd0d4037c0a5a16da5b03a2811369917c77520
roma <mavlutovr@ya.ru>
2022-01-20 19:08:32 +0300

    Repo initialization

---
+++ README.md
@@ -0,0 +1,61 @@
+# toggle-on-click-jquery
+
+- Opens by
+  - Click
+- Closes by
+  - Second click
+  - Mousedown outside
+  - Esc
+
+![](https://i.imgur.com/gxxFfAn.gif)
+
+## Demo
+
+https://webdeveloper.pro/demo/toggle-on-click-jquery/demo.html
+
+## Install
+
+```shell
+npm install toggle-on-click-jquery --save
+```
+
+## Setup
+
+```javascript
+import toggleOnClick from 'toggle-on-click-jquery';
+```
+
+## Usage
+
+```html
+<p><button id="js-button">Click here</button></p>
+	
+<div class="menu" id="js-menu">
+    Menu or something else...
+</div>
+```
+
+```javascript
+$(document).ready(function () {
+    toggleOnClick(
+        $('#js-button'), // Button
+        $('#js-menu'), // Target
+        {
+            // How to open (optional)
+            open($element) {
+                $element.slideDown(200);
+            },
+
+            // How to close (optional)
+            close($element) {
+                $element.slideUp(200);
+            }
+        }
+    );
+})
+```
+
+## License
+
+MIT
+
---
+++ index.js
@@ -0,0 +1,44 @@
+const toggleOnClick = ($button, $target, { open, close }) => {
+  let canOpen = true;
+
+  if (!close) close = $target => $target.fadeOut(150);
+  if (!open) open = $target => $target.fadeIn(150);
+
+  $button.on('click', () => {
+
+    if (!canOpen) return;
+    canOpen = false;
+    open($target);
+
+    let mouseOnTargetOvered = false;
+    $target.on('mouseover', () => mouseOnTargetOvered = true);
+    $target.on('mouseout', () => mouseOnTargetOvered = false);
+
+    const closeStart = () => {
+      close($target);
+      document.removeEventListener('mousedown', closeByMouse);
+      window.removeEventListener('keydown', closeByEscape);
+      setTimeout(() => canOpen = true, 200);
+    };
+
+    $target.on('toggleOnClickClose', () => {
+      closeStart();
+    });
+
+    const closeByMouse = e => {
+      if (!mouseOnTargetOvered) {
+        e.preventDefault();
+        closeStart();
+        return false;
+      }
+    };
+    document.addEventListener('mousedown', closeByMouse);
+    const closeByEscape = e => {
+      if (e.keyCode === 27) closeStart();
+    };
+    window.addEventListener('keydown', closeByEscape);
+  });
+};
+
+
+export default toggleOnClick;
